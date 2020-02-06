de5162fce9f2b04d11b47715b6067bc2abfc3ff0
brountree22 <brountree22@nscds.org>
2020-02-06 17:34:50 +0000

    tictactoe

--- TicTacToe/functions.js
+++ TicTacToe/functions.js
@@ -1,58 +1,54 @@
-var i = 2;
-var player = 0
+var i = 5;
+var player = "";
+var turn = player
 
-$('.start').click(
+
+$('.x').click(
     function(){
-        i += 1
-    console.log(i)
+        i += 1;
+    console.log(i);
     }
 );
 
-for (e=0;e=0;e=0){
-    if ($('.box').click){
-        console.log("idk")
+function assign (player){   
+    if ( (i % 2) == 0 ) {
+        player = "x";
+        i++;  
     }
-}
+    else {
+        player = "o";
+        i++;     
+    }
+    console.log(player);
+    return turn = player;
+}  
 
-function assign (player){
-    $(".box").click( 
-        function (){
-        if ( (i % 1) == 0 ) {
-            var player = 1
-            i++
-            return player
-        }
-        else {
-            var player = 2
-            i++
-            return player
-        } 
-    }   
-    );
-    console.log(player)
-}
 
-function xoro(thing) {  
-    if (player == 1){
-        document.getElementById(thing).innerHTML = X
+function xoro(thing) { 
+    console.log(turn);
+    //var id = thing.att("id") 
+    if (turn == "o"){
+        $(thing).text("O");
+        console.log("o worked");
     }
     else  {
-        if (player ==2) {
-            document.getElementById(thing).innerHTML = O
+        if (turn == "x") {
+            $(thing).text("X");
+            console.log("X worked");
         }
         else {
-            console.log("something went wrong making it x or o")
+            console.log("something went wrong making it x or o");
         }
     }
 }
 
-for (t = 1; t <= 9; t++){
-    $('.box').click(
-    function(){
-        assign(player)
-        xoro($(this))
-        }
-    );
-}
+
+//for (var t = 1; t <= 9; t++){
+$('.box').click(    
+function(){
+    assign(player);
+    xoro($(this));
+    }
+)
 
  
