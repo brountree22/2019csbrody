f4290dbb60efac5fa59e6312059f57c694d54751
brountree22 <brountree22@nscds.org>
2020-02-10 17:01:36 +0000

    tictactoe

--- TicTacToe/functions.js
+++ TicTacToe/functions.js
@@ -1,7 +1,8 @@
 var i = 5;
 var player = "";
-var turn = player
-
+var turn = player;
+var t = 0;
+var board = [('.1'),('.2'),('.3'),('.4'),('.5'),('.6'),('.7'),('.8'),('.9')];
 
 $('.x').click(
     function(){
@@ -46,20 +47,20 @@ function xoro(thing) {
     //var id = thing.att("id")
     if (turn == "o"){
         if (thing.attr("clicke") == "false") {
-            $(thing).text("O");
+            $(thing).text(" O");
             thing.attr("clicke","true")
         console.log("o worked");
         }
-        else {console.log("already clicked")}
+        else {console.log("already clicked");i--}
     }
     else  {
         if (turn == "x") {
             if (thing.attr("clicke") == "false") {
-                $(thing).text("X");
+                $(thing).text(" X");
                 thing.attr("clicke","true")
             console.log("X worked");
             }
-            else {console.log("already clicked")}
+            else {console.log("already clicked");i--}
         }
         else {
             console.log("something went wrong making it x or o");
@@ -72,7 +73,22 @@ $('.box').click(
 function(){
     assign(player);
     xoro($(this));
+    checkWin();
+    t++
     }
 )
+
+var metone = board[0] + board[3] + board[6];
+
+function checkWin() {
+    if (metone.indexOf("x") == 3){
+        alert("X wins!")
+    }
+    else{
+        console.log("metone" + metone);
+    }
+}
+
+
 
  
