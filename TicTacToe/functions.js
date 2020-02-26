4658e54184a6da670f9954dd9b922bb78dee54d6
Brody Rountree <brountree22@nscds.org>
2020-02-26 19:06:47 +0000

    Rewind from 5 days ago.

--- TicTacToe/functions.js
+++ TicTacToe/functions.js
@@ -1,45 +1,37 @@
-var i = undefined;
+var i = 5;
 var player = "";
 var turn = player;
 var t = 0;
+/*var board = [$('#0'),$('#1'),$('#2'),
+             $('#3'),$('#4'),$('#5'),
+             $('#6'),$('#7'),$('#8')];
+             */
 
 $('.x').click(
     function(){
-        i += 0;
+        i += 1;
     }
 );
-
-$('.o').click(
-    function(){
-        i=3
-    }
-)
-
-$('.reset').click(
-
-)
 
 function assign (player){   
     if ( (i % 2) == 0 ) {
         player = "x";
         i++;  
     }
-    if ((i%2)==1){
+    else {
         player = "o";
         i++;     
-    }
-    else{
-        alert("Click a button to start please!!")
     }
     return turn = player;
 }  
 
 function xoro(thing) { 
+    //var id = thing.att("id")
     if (turn == "o"){
         if (thing.attr("clicke") == "false") {
             $(thing).text("O");
             thing.attr("clicke","o")
-            thing.attr("p","O")
+            thing.attr("p","0")
         console.log("o worked");
         }
         else {console.log("already clicked");i--}
@@ -49,7 +41,7 @@ function xoro(thing) {
             if (thing.attr("clicke") == "false") {
                 $(thing).text("X");
                 thing.attr("clicke","x");
-                thing.attr("p","X")
+                thing.attr("p","1")
             console.log("X worked");
             }
             else {console.log("already clicked");i--}
@@ -60,23 +52,35 @@ function xoro(thing) {
     }
 }
 
+//for (var t = 1; t <= 9; t++){
 $('.box').click(    
 function(){
     assign(player);
-    if (player)
     xoro($(this));
+    //console.log(acrosstop)
     checkWin(this);
     t++
     }
 )
 
+/*
+var acrosstop = $("[row = 1]");
+var acrossmiddle = $("[row = 2]");
+var acrossbottom = $("[row = 3]");
+var downa = $("[collumn = a]");
+var downb = $("[collumn = b]");
+var downc = $("[collumn = c]");
+*/
+
+var boxing = [];
+
 function checkWin(boxclicked) {
     var rowclicked = $(boxclicked).attr("row");
-    var collumnclicked = $(boxclicked).attr("collumn");
+    var collumnclicked = $(boxclicked).attr("collumn")
     var wplayer = $(boxclicked).attr("p");
     var roww = $("[row="+rowclicked+"][p="+wplayer+"]");
     var coll = $("[collumn="+collumnclicked+"][p="+wplayer+"]");
-    console.log(rowclicked,collumnclicked,wplayer);
+    console.log(rowclicked,collumnclicked,wplayer)
     console.log(roww.length)
     if (roww.length == 3){
         alert("Player " + wplayer + " Wins!")
@@ -85,3 +89,38 @@ function checkWin(boxclicked) {
         alert("Player " + wplayer + " Wins!")
     }
 }
+    
+    /*checkingrow = ("div[row="+rowclicked+"]").attr("clicke");
+    var str = $(checkingrow[0]).text() + $(checkingrow[1]).text() + $(checkingrow[2]).text();
+    console.log(checkingrow)
+    console.log(str)
+        if (wplayer == 0){
+            if (str == "ooo"){
+                alert("Player O Wins!");
+            }
+            else {return}
+        }
+        else if (wplayer == 1){
+            //player x checking
+        }   
+            if(str == "xxx"){
+                alert("Player X Wins!");
+            }
+        else {
+            console.log("not clicked yet");
+        }
+    
+    //var yesno = $(checkingrow).val();
+
+// I want it to select all of the <div> elements with attributes equal to the value of the variable rowclicked. (ln 75)
+// Then, I want it to return an array of each selected option. (ln 76)
+// If the returned array is equal to a winning array, 
+// The game is won.
+
+    console.log(rowclicked);
+    console.log(checkingrow);
+    //console.log(yesno);*/
+
+
+ // https://api.jquery.com/val/
+ // https://www.tutorialspoint.com/How-to-use-JavaScript-variables-in-jQuery-selectors
